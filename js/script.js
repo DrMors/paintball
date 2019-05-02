72067afc0717bbb50ad8fe7dd5bc8f9be27e10b4
Alexey Kalinin <dr.fupfup@gmail.com>
2019-05-02 22:17:05 +0300

    добавил слайдер  и плавный переход

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
-// боковое меню навигации
 var mobileMenuActiveClass = 'nav_mobile-active'
-var burgerButton = document.querySelector('.burger_btn')
-var mobileMenu = document.querySelector('.nav_mobile')
-var mobileMenuBg = document.querySelector('.nav_mobile__bg')
-var navLinks = document.querySelectorAll('.head_nav__link')
 
-function showMenu () {
-    mobileMenu.classList.add(mobileMenuActiveClass)
-}
+$('.burger_btn').on('click', function () {
+    $('.nav_mobile').addClass(mobileMenuActiveClass)
+})
 
-function hideMenu () {
-    mobileMenu.classList.remove(mobileMenuActiveClass)
-}
 
-burgerButton.addEventListener('click', showMenu)
-mobileMenuBg.addEventListener('click', hideMenu)
+$('.nav_mobile__bg').on('click', function () {
+    $('.nav_mobile').removeClass(mobileMenuActiveClass)
+})
 
-//  плавный переход по якорям (не работает)
-for (var i = 0; i < navLinks.length; i++) {
-    var link = navLinks[i]
-    var href = link.getAttribute('href')
-    var id = href.substring(1)
 
-    app.scrollTo(link)
-}
+$('.main_slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    accessibility: false,
+    autoplay: true,
+    prevArrow: '<button class="main_slider__arrow left"><img src="Paintball/arrow-left.png" alt="arrow left"></button>',
+    nextArrow: '<button class="main_slider__arrow right"><img src="Paintball/arrow-left.png" alt="arrow right"></button>',
+    pauseOnHover: false
+})
+
+
+$('.head_nav__link').on('click', function (e) {
+    e.preventDefault()
+  
+    $('html').animate({
+        scrollTop: $($(this).attr('href')).offset().top,
+    }, 500)
+})
